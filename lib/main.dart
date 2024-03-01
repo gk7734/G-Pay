808bda569e0a60aad38794fe597eff02edc3d266
gk7734 <gamekiller7734@naver.com>
2024-03-01 14:41:51 +0900

    add to Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
-import 'package:toonflix/widgets/button.dart';
+import 'package:GPay/widgets/button.dart';
 
 void main() {
   runApp(const App());
@@ -14,85 +15,148 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: Platform.isAndroid ? 50 : 60,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          "Hey, GameKiller",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                          ),
+        backgroundColor: const Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: Platform.isAndroid ? 50 : 60,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Hey, GameKiller",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
                         ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white.withOpacity(0.8),
-                          ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white.withOpacity(0.8),
                         ),
-                      ],
-                    )
-                  ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: Platform.isAndroid ? 110 : 120,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  fontSize: Platform.isAndroid ? 20 : 22,
+                  color: Colors.white.withOpacity(0.8),
                 ),
-                const SizedBox(
-                  height: 120,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "\$5 179 812",
+                style: TextStyle(
+                  fontSize: Platform.isAndroid ? 38 : 48,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white.withOpacity(0.8),
                 ),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: Platform.isAndroid ? 20 : 22,
-                    color: Colors.white.withOpacity(0.8),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Button(
+                    text: "Transfer",
+                    bgColor: Color(0xFFF1B33B),
+                    textColor: Colors.black,
+                  ),
+                  Button(
+                    text: "Request",
+                    bgColor: Color(0xFF1F2123),
+                    textColor: Colors.white,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: Platform.isAndroid ? 90 : 100,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Wallets",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: Platform.isAndroid ? 12 : 8),
+                    child: Text(
+                      "View All",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: Platform.isAndroid ? 15 : 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFF1F2123),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Euro",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: Platform.isAndroid ? 28 : 32,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Row(
+                            children: [
+                              Text("6 428"),
+                              Text("EUR"),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "\$5 179 812",
-                  style: TextStyle(
-                    fontSize: Platform.isAndroid ? 38 : 45,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white.withOpacity(0.8),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: "Transfer",
-                      bgColor: Color(0xFFF1B33B),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: "Request",
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-
-                ),
-              ],
-            ),
-          )),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
